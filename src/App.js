239a124f78464f3498a3c9925322bf13f9700888
mathurdevanshi <mathurdevanshi@gmail.com>
2019-06-12 18:25:59 -0700

    it kinda works

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from "react";
-import Card from "./components/Wrapper";
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
 import Header from "./components/Header";
 import cards from "./cards.json";
 import "./App.css";
